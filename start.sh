291dce8764c793682c2f719ce6c94f7939e6fefb
wroxen-2nd <wroxen3rd@proton.me>
2025-10-18 13:38:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
-# Start health server in background
+#!/bin/bash
+
+# Activate uv virtual environment
+source /app/.venv/bin/activate
+
+# Start Flask health server in the background
 python3 health.py &
 
 # Start your main scripts
