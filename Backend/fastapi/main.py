34ce4b3296dc3f0e92f1900917d841a681fca21a
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-10-18 14:08:50 +0530

    Update main.py

--- Backend/fastapi/main.py
+++ Backend/fastapi/main.py
@@ -140,12 +140,12 @@ async def auth_exception_handler(request: Request, exc):
 
 from flask import Flask
 
-app = Flask(__name__)
+apps = Flask(__name__)
 
-@app.route("/healthz")
+@apps.route("/healthz")
 def health_check():
     return "OK", 200
 
 if __name__ == "__main__":
     # Bind to all interfaces and use port 8000
-    app.run(host="0.0.0.0", port=8080)
+    apps.run(host="0.0.0.0", port=8000)
